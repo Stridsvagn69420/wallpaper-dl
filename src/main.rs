0b04a160f8f0a6f0bb5bf98f72e0a4a7d02b5321
Stridsvagn69420 <Stridsvagn69420@users.noreply.github.com>
2024-03-12 01:09:30 +0100

    Simple test to try out scraper and reqwest
    Test obviously does not include any filesystem I/O yet, and I will
    remove the test shortly after this commit. I don't know a proper structure
    yet, but I do know that I want to support these sites so far:
    - alphacoders.com and their subdomains (HTML structure seems similar for
      them)
    - wallhaven.cc (just look at this commit)
    I could probably make use of Traits here. Might as well make this a
    binary-library crate then.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::env;
+use apputils::console::Colors;
+use apputils::paintln;
+
+mod test;
+
+fn main() -> Result<(), Box<dyn Error>> {
+	let args: Vec<String> = env::args().collect();
+	if args.len() < 2 {
+		paintln!(Colors::Red, "No URLs provided!");
+		return Ok(());
+	}
+	for url in args.into_iter().skip(1) {
+		paintln!(Colors::Magenta, "URL: {}", url);
+		let _ = test::wallhaven_test(&url);
+	}
+	Ok(())
 }
