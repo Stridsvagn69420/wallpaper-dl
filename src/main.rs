2730d0374adf2b8c4334ca61700f302ce7fb5562
Stridsvagn69420 <Stridsvagn69420@users.noreply.github.com>
2024-06-18 21:52:23 +0200

    At least change the output format

--- src/main.rs
+++ src/main.rs
@@ -135,9 +135,9 @@ fn download(client: &Client, url: Url, id: &str, host: &str, ctr: (bool, u8)) ->
 
 	// Build filename
 	let fname = if ctr.0 {
-		format!("{id}_{host}.{ext}")
+		format!("{host}_{id}.{ext}")
 	} else {
-		format!("{id}_{}_{host}.{ext}", ctr.1)
+		format!("{host}_{id}_{}.{ext}", ctr.1)
 	};
 
 	// Download file data
